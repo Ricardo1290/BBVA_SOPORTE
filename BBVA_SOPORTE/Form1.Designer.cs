ba731cfb700b281577441265e63ef64e886d14db
Ricardo Javier <Ricardopascasio12@hotmail.com>
2018-07-17 21:42:24 -0500

    BBVA_SOPORTE

---
+++ BBVA_SOPORTE/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace BBVA_SOPORTE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPasajero = new System.Windows.Forms.TextBox();
+            this.txtViaje = new System.Windows.Forms.TextBox();
+            this.txtMotivo = new System.Windows.Forms.TextBox();
+            this.txtDuracion = new System.Windows.Forms.TextBox();
+            this.txtConsulta = new System.Windows.Forms.TextBox();
+            this.txtComentarios = new System.Windows.Forms.TextBox();
+            this.dtp1 = new System.Windows.Forms.DateTimePicker();
+            this.ErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.btnAgregar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ErrorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 323);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(101, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "COMENTARIOS:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 278);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "CONSULTA:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 232);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(137, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "DURACION LLAMADA:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "MOTIVO LLAMADA:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "N°VIAJE:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "PASAJERO:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "FECHA:";
+            // 
+            // txtPasajero
+            // 
+            this.txtPasajero.Location = new System.Drawing.Point(202, 83);
+            this.txtPasajero.Multiline = true;
+            this.txtPasajero.Name = "txtPasajero";
+            this.txtPasajero.Size = new System.Drawing.Size(221, 25);
+            this.txtPasajero.TabIndex = 14;
+            // 
+            // txtViaje
+            // 
+            this.txtViaje.Location = new System.Drawing.Point(202, 128);
+            this.txtViaje.Multiline = true;
+            this.txtViaje.Name = "txtViaje";
+            this.txtViaje.Size = new System.Drawing.Size(221, 26);
+            this.txtViaje.TabIndex = 15;
+            // 
+            // txtMotivo
+            // 
+            this.txtMotivo.Location = new System.Drawing.Point(202, 174);
+            this.txtMotivo.Multiline = true;
+            this.txtMotivo.Name = "txtMotivo";
+            this.txtMotivo.Size = new System.Drawing.Size(221, 45);
+            this.txtMotivo.TabIndex = 16;
+            // 
+            // txtDuracion
+            // 
+            this.txtDuracion.Location = new System.Drawing.Point(202, 225);
+            this.txtDuracion.Multiline = true;
+            this.txtDuracion.Name = "txtDuracion";
+            this.txtDuracion.Size = new System.Drawing.Size(221, 28);
+            this.txtDuracion.TabIndex = 17;
+            // 
+            // txtConsulta
+            // 
+            this.txtConsulta.Location = new System.Drawing.Point(202, 271);
+            this.txtConsulta.Multiline = true;
+            this.txtConsulta.Name = "txtConsulta";
+            this.txtConsulta.Size = new System.Drawing.Size(221, 46);
+            this.txtConsulta.TabIndex = 18;
+            // 
+            // txtComentarios
+            // 
+            this.txtComentarios.Location = new System.Drawing.Point(202, 323);
+            this.txtComentarios.Multiline = true;
+            this.txtComentarios.Name = "txtComentarios";
+            this.txtComentarios.Size = new System.Drawing.Size(221, 47);
+            this.txtComentarios.TabIndex = 19;
+            // 
+            // dtp1
+            // 
+            this.dtp1.CustomFormat = "dd-MM-yyyy";
+            this.dtp1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtp1.Location = new System.Drawing.Point(202, 37);
+            this.dtp1.Name = "dtp1";
+            this.dtp1.Size = new System.Drawing.Size(97, 20);
+            this.dtp1.TabIndex = 20;
+            // 
+            // ErrorProvider
+            // 
+            this.ErrorProvider.ContainerControl = this;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(507, 181);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(122, 38);
+            this.btnAgregar.TabIndex = 21;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 396);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dtp1);
+            this.Controls.Add(this.txtComentarios);
+            this.Controls.Add(this.txtConsulta);
+            this.Controls.Add(this.txtDuracion);
+            this.Controls.Add(this.txtMotivo);
+            this.Controls.Add(this.txtViaje);
+            this.Controls.Add(this.txtPasajero);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "                                                                                 " +
+    "BBVA-SOPORTE-SISTEMAS";
+            ((System.ComponentModel.ISupportInitialize)(this.ErrorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPasajero;
+        private System.Windows.Forms.TextBox txtViaje;
+        private System.Windows.Forms.TextBox txtMotivo;
+        private System.Windows.Forms.TextBox txtDuracion;
+        private System.Windows.Forms.TextBox txtConsulta;
+        private System.Windows.Forms.TextBox txtComentarios;
+        private System.Windows.Forms.DateTimePicker dtp1;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ErrorProvider ErrorProvider;
+    }
+}
+
---
+++ BBVA_SOPORTE/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Text.RegularExpressions;
+
+namespace BBVA_SOPORTE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ValidarCampos();
+
+                string ConStr = "";
+                //getting the path of the file    
+
+                //string path = Server.MapPath("InsertDatos.xlsx");
+                string fileName = "InformeDiario.xlsx";
+
+                string ruta = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+                string PathArchivo = Path.Combine(ruta, fileName);
+                //connection string for that file which extantion is .xlsx    
+                ConStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + PathArchivo + ";Extended Properties=\"Excel 12.0;ReadOnly=False;HDR=Yes;\"";
+                //making query
+                string query = "INSERT INTO [Hoja1$]([Fecha], [Pasajero], [Nº Viaje], [Motivo de la llamada], [Duración de la llamada],[Es consulta de soporte navegacional], [Comentarios])" +
+                    " VALUES('" + dtp1.Text + "','" + txtPasajero.Text + "','" + txtViaje.Text + "','" + txtMotivo.Text + "','"
+                        + txtDuracion.Text + "','" + txtConsulta.Text + "','" + txtComentarios.Text + "')";
+                //Providing connection     
+                OleDbConnection conn = new OleDbConnection(ConStr);
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                //create command object    
+                OleDbCommand cmd = new OleDbCommand(query, conn);
+                int result = cmd.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    MessageBox.Show("SE AGREGARON LOS DATOS SATISFACTORIAMENTE", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    txtComentarios.Text = "";
+                    txtConsulta.Text = "";
+                    txtDuracion.Text = "";
+                    txtMotivo.Text = "";
+                    txtPasajero.Text = "";
+                    txtViaje.Text = "";
+
+                }
+                
+                conn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("POR FAVOR INTENTE DE NUEVO", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private bool ValidarCampos()
+        {
+            Regex rx = new Regex(@"^[+-]?\d+(\.\d+)?$");
+            bool ok = true;
+
+            if (rx.IsMatch(txtViaje.Text))
+            {
+                ok = false;
+                ErrorProvider.SetError(txtPasajero, "INGRESE PASAJARE");
+
+            }
+
+            if (txtViaje.Text == "")
+            {
+                ok = false;
+                ErrorProvider.SetError(txtViaje, "INGRESE N°VIAJE");
+
+            }
+            if (txtMotivo.Text == "")
+            {
+                ok = false;
+                ErrorProvider.SetError(txtMotivo, "INGRESE MOTIVO LLAMADA");
+
+            }
+            if (txtDuracion.Text == "")
+            {
+                ok = false;
+                ErrorProvider.SetError(txtDuracion, "INGRESE DURACION LLAMADA");
+
+            }
+
+            if (txtConsulta.Text == "")
+            {
+                ok = false;
+                ErrorProvider.SetError(txtConsulta, "INGRESE CONSULTA");
+
+            }
+            if (txtComentarios.Text == "")
+            {
+                ok = false;
+                ErrorProvider.SetError(txtComentarios, "INGRESE COMENTARIOS");
+
+            }
+            return ok;
+        }
+
+    }
+}
